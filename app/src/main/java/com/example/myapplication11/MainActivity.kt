0c75d5d2f78b8e57d5e91897cbb904aacb32ce97
nati050 <natan.zit@gmail.com>
2023-06-05 22:19:00 +0300

    constraint layout work

--- app/src/main/java/com/example/myapplication11/MainActivity.kt
+++ app/src/main/java/com/example/myapplication11/MainActivity.kt
@@ -20,42 +20,16 @@ import java.util.logging.Logger
 
 class MainActivity : ComponentActivity() {
     var logger = Logger.getLogger("MainActivity")
-    lateinit var  edittext:TextView
-    lateinit  var btn:Button
-    lateinit var emailtext:TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-
-
-         edittext=findViewById<TextView>(R.id.textView4)
-
-          emailtext=findViewById<EditText>(R.id.editTextTextEmailAddress2)
-         btn=findViewById(R.id.button2)
-
-
-        btn.setOnClickListener ({logger.log(Level.WARNING,"My first view"  +"edittext"+ edittext.text  + emailtext.text)  })
+        setContentView(R.layout.main_activity_constraint)
     }
 
     override fun onStop() {
         super.onStop()
         logger.log(Level.WARNING,"My first view")
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplication11Theme {
-        Greeting("Android")
-    }
 }
